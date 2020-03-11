6cd800c8d1537f711251519876ee199e66a2c362
kiran ram <kram@lions.molloy.edu>
2020-03-11 01:04:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
     
   end
   
-  def self.new_from_filename
+  def self.new_from_filename(name)
     song = self.new 
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
