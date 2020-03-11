f39395ab3c8478df92615ebf1aa23f612d3f8044
kiran ram <kram@lions.molloy.edu>
2020-03-11 01:03:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,11 +59,19 @@ class Song
     @@all << song
     song
   end
+  
+  def self.create_from_filename(name)
+  #class method should not only parse the filename correctly but should also save the song
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
 
     
     
   def self.destroy_all
-       @@all = []
+       @@all.clear
   end
   
 end
